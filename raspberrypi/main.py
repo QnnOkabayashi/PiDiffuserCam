324a01fb1c9ab0bbafbb5a572561977db49f016b
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-20 20:18:05 -0800

    Started camera interfacing

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,4 +1,5 @@
-import os, picamera
+import os, picamera.PiCamera
+from datetime import datetime
 
 # Create '/captures' for storing raw images if it doesn't already exist
 captures_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "captures")
@@ -7,4 +8,9 @@ if not os.path.isdir(captures_path):
 
 # Do image capturing stuff here
 
+with PiCamera() as camera:
+    d = datetime.now()
+    label = f"{d.year}-{d.month}-{d.day} at {d.hour}.{d.minute}.{d.second}"
+
+    camera.capture(output=f"captures/{label}.png")
 
